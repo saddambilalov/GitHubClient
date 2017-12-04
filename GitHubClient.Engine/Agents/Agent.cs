63b2ea61720935523430062f78f7caa555e5d13a
Saddam Bilalov <saddam.bilalov@gmail.com>
2017-12-04 22:35:44 +0100

    Unit Test implemented to test

--- GitHubClient.Engine/Agents/Agent.cs
+++ GitHubClient.Engine/Agents/Agent.cs
@@ -1,11 +1,17 @@
 ﻿using System;
+using System.Threading.Tasks;
 using GitHubClient.Infracture.Models;
 
 namespace GitHubClient.Engine.Agents
 {
     public class Agent
     {
-        public UserApiResult GetUserApiResult(string userName)
+        public async Task<UserApiResult> GetUserInfoWithRepositoriesAsync(string userName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<UserApiResult> FilterRepositoriesByStargazersCount(UserApiResult userApiResult, int stargazersCount)
         {
             throw new NotImplementedException();
         }
---
+++ GitHubClient.UnitTests/GitHubClientUnitTests.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using GitHubClient.Engine.Agents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GitHubClient.UnitTests
+{
+    [TestClass]
+    public class GitHubClientUnitTests
+    {
+        [TestMethod]
+        public async Task GetUserInfoWithRepositoriesAsync_CorrectUser()
+        {
+            var userApiResult = await new Agent().GetUserInfoWithRepositoriesAsync("saddambilalov");
+
+            Assert.IsNotNull(userApiResult);
+            Assert.IsNotNull(userApiResult.UserInfo);
+            Assert.AreEqual(userApiResult.UserInfo.Name, "Saddam Bilalov");
+        }
+
+        [TestMethod]
+        public async Task GetUserInfoWithRepositoriesAsync_Top5_Repositories_With_Stargazers_Count()
+        {
+            var userApiResult = await new Agent().GetUserInfoWithRepositoriesAsync("saddambilalov");
+            Assert.IsNotNull(userApiResult);
+            Assert.IsNotNull(userApiResult.Repositories);
+
+            var userApiResultFiltered = await new Agent().FilterRepositoriesByStargazersCount(userApiResult, 5);
+
+            Assert.IsTrue(userApiResultFiltered.Repositories.Count > 5, "The count of repositories could not be greater than five");
+        }
+    }
+}
