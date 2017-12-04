01d82a769933b54a3db0f2337683364748666c5f
Saddam Bilalov <saddam.bilalov@gmail.com>
2017-12-04 21:58:15 +0100

    Agent class implemented to parse data from api

---
+++ GitHubClient.Engine/Agents/Agent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using GitHubClient.Infracture.Models;
+
+namespace GitHubClient.Engine.Agents
+{
+    public class Agent
+    {
+        public UserApiResult GetUserApiResult(string userName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
