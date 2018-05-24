b4bdfcf83aaf725630b611358c3562cb0ab5099a
Saddam Bilalov <saddam.bilalov@outlook.com>
2018-05-24 22:48:17 +0200

    the Unit tests moved to NUnit.

--- GitHubClient.UnitTests/GitHubClientUnitTests.cs
+++ GitHubClient.UnitTests/GitHubClientUnitTests.cs
@@ -1,32 +1,46 @@
 ﻿using GitHubClient.Engine;
 using GitHubClient.Engine.Agents;
 using GitHubClient.Engine.Filters;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading.Tasks;
 using GitHubClient.Engine.Parsers;
+using NUnit.Framework;
 
 namespace GitHubClient.UnitTests
 {
-	[TestClass]
-	public class GitHubClientUnitTests
-	{
-		[TestMethod]
-		public async Task GetUserInfoWithRepositoriesAsync_CorrectUser()
-		{
-			var userApiResult = await new GitHubAgent(AppSettings.BaseUrl, new JsonParser()).GetUserInfoWithRepositoriesAsync("saddambilalov");
+    public class GitHubClientUnitTests
+    {
+        private GitHubAgent _gitHubAgent;
 
-			Assert.IsNotNull(userApiResult.UserInfo);
-			Assert.AreEqual(userApiResult.UserInfo.Name, "Saddam Bilalov");
-		}
+        [SetUp]
+        public void SetUp()
+        {
+            _gitHubAgent = new GitHubAgent(AppSettings.BaseUrl, new JsonParser());
+        }
 
-		[TestMethod]
-		public async Task GetUserInfoWithRepositoriesAsync_Top5_Repositories_With_Stargazers_Count()
-		{
-			var userApiResult = await new GitHubAgent(AppSettings.BaseUrl, new JsonParser()).GetUserInfoWithRepositoriesAsync("saddambilalov");
-			Assert.IsNotNull(userApiResult.Repositories);
+        [TearDown]
+        public void TearDown()
+        {
+            _gitHubAgent = null;
+        }
 
-			var userApiResultFiltered = RepositoryFilters.FilterRepositoriesByStargazersCount(userApiResult, 5);
-			Assert.IsFalse(userApiResultFiltered.Repositories.Count > 5, "The count of repositories could not be greater than 5");
-		}
-	}
+        [TestCase("saddambilalov", "Saddam Bilalov")]
+        [TestCase("thedevsaddam", "Saddam H")]
+        public async Task GetUserInfoWithRepositoriesAsync_CorrectUser(string input, string expectedOutput)
+        {
+            var userApiResult = await _gitHubAgent.GetUserInfoWithRepositoriesAsync(input);
+
+            Assert.IsNotNull(userApiResult.UserInfo);
+            Assert.That(userApiResult.UserInfo.Name, Is.EqualTo(expectedOutput));
+        }
+
+        [Test]
+        public async Task GetUserInfoWithRepositoriesAsync_Top5_Repositories_With_Stargazers_Count()
+        {
+            var userApiResult = await _gitHubAgent.GetUserInfoWithRepositoriesAsync("saddambilalov");
+            Assert.That(userApiResult.Repositories, Is.Not.Null);
+
+            var userApiResultFiltered = RepositoryFilters.FilterRepositoriesByStargazersCount(userApiResult, 5);
+            Assert.That(userApiResultFiltered.Repositories.Count > 5, Is.Not.EqualTo("The count of repositories could not be greater than 5"));
+        }
+    }
 }
